5f7706e8589140a153aba9643f0e018c83a39aec
Edouardoo <rouzedouard@yahoo.fr>
2024-11-26 10:40:01 +0100

    compil features lib.rs + bump_alloc main.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,4 @@
 #![no_std]
+#![feature(allocator_api)]
+#![feature(const_mut_refs)]
+#![cfg_attr(feature = "global_alloc", feature(global_allocator))]
---
+++ src/main.rs
@@ -0,0 +1,42 @@
+#[cfg(feature = "global_alloc")]
+extern crate alloc;
+
+#[cfg(feature = "global_alloc")]
+use core::alloc::{GlobalAlloc, Layout};
+
+
+
+
+
+#[cfg(feature = "global_alloc")]
+
+pub struct BumpAllocator {
+    heap_start: usize,
+    heap_end: usize,
+    next: usize,
+}
+
+impl BumpAllocator {
+    pub const unsafe fn new(heap_start: usize, heap_size: usize) -> Self {
+        BumpAllocator {
+            heap_start,
+            heap_end: heap_start + heap_size,
+            next: heap_start,
+        }
+    }
+
+    unsafe fn alloc(&mut self, layout: Layout) -> *mut u8 {
+        let alloc_start = align_up(self.next, layout.align());
+        let alloc_end = alloc_start + layout.size();
+
+
+        if alloc_end > self.heap_end {
+            core::ptr::null_mut()
+        } else {
+            self.next = alloc_end;
+            alloc_start as *mut u8
+        }
+    }
+}
+
+
